aa5d55ccfe05abb1d68235297f15612e86bd0f2a
Вячеслав Тен <slavat0312@gmail.com>
2026-01-20 23:36:18 +0300

    начало напоминалок

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,48 +29,64 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlin {
         jvmToolchain(17)
     }
+
     buildFeatures {
         compose = true
     }
 }
 
 dependencies {
+    implementation(platform(libs.androidx.compose.bom))
 
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
     implementation(libs.androidx.lifecycle.viewmodel.navigation3)
     implementation(libs.androidx.material3.adaptive.navigation3)
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.navigation3.ui)
+    implementation(libs.androidx.navigation.compose)
+
     implementation(libs.elmslie.android)
+
     implementation(libs.kotlinx.serialization.core)
-    implementation(platform(libs.androidx.compose.bom))
+
     implementation(libs.okhttp)
     implementation(libs.retrofit)
-    implementation(libs.androidx.navigation.compose)
-    implementation(libs.coil.compose)
-    implementation(libs.coil.network.okhttp)   // <-- required for http/https
 
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp) // required for http/https
 
     implementation(libs.dagger)
     ksp(libs.dagger.compiler) // Dagger compiler
+
+
+    // для напоминалок
+    implementation("androidx.work:work-runtime-ktx:2.9.0")              // WorkManager
+    implementation("androidx.datastore:datastore-preferences:1.1.1")   // DataStore (local persistence)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
---
+++ app/src/main/java/ru/misterpotz/demo/domain/repositories/ReminderRepository.kt
@@ -0,0 +1,16 @@
+package ru.misterpotz.demo.domain.repositories
+
+import kotlinx.coroutines.flow.Flow
+import ru.misterpotz.demo.domain.models.Reminder
+
+interface ReminderRepository {
+    fun observeAll(): Flow<List<Reminder>>// показывает все заметки
+    suspend fun getByMediaId(mediaId: String): List<Reminder>//берётся весь список напоминаний и фильтруется по mediaId
+
+    suspend fun add(reminder: Reminder)/*две задачи:
+    1)сохраняет напоминание локально в нашем хранилище
+    2)Ставит WorkManager задачу чтобы чтобы уведомление пришло в нужное время */
+
+    suspend fun delete(reminderId: String)/*так же две задачи: 1)удалить из локального хранилища
+    2)Отменить WorkManager задачу*/
+}
